0947db3d418ae140a5868093abca5146921a8886
Matt Wilson <TheMagicCat@users.noreply.github.com>
2018-02-25 15:54:53 -0600

    in progress

--- js/main.js
+++ js/main.js
@@ -1,96 +1,45 @@
 (function () {
-	function transformString(v) {
-		return `translate3d(${v.tX}px, ${v.tY}px, ${v.tZ}px) rotateX(${v.rX}deg) rotateY(${v.rY}deg) rotateZ(${v.rZ}deg) scale(${v.s})`;
-	}
+	const transformString = (v) => `translate3d(${v.x}px, ${v.y}px, 0px) scale(1)`;
 
-	function move(scale, bound, pos) {
-		return ((scale * 2) / bound) * pos - scale;
-	}
+	// All this math does is bound + damp the transformations! Can also be done with min/max.
+	const move = (scale, bound, pos) => ((scale * 2) / bound) * pos - scale;
 
-	const defaultOpts = {
-		movement: {
-			image: {
-				translation: { x: -12, y: -12, z: 0 },
-				rotation: { x: 0, y: 0, z: 0 },
-				scale: 1.1,
-			},
-			lines: {
-				translation: { x: 5, y: 5, z: 0 },
-				rotation: { x: 0, y: 0, z: 0 },
-				scale: 1,
-			},
-		},
-	};
+	const init = (magnitude, root) => {
 
-	class TiltFx {
-		constructor(el, options) {
-			this.mouseleaveFn = this.mouseleaveFn.bind(this);
-			this.mousemoveFn = this.mousemoveFn.bind(this);
-			this._layout = this._layout.bind(this);
-
-			this.DOM = { el };
-			this.options = Object.assign({}, defaultOpts, options);
-			this.DOM.animatable = {};
-			this.DOM.animatable.image = this.DOM.el.querySelector('.subject');
-			// this.DOM.animatable.lines = this.DOM.el.querySelector('.lines');
-
-			console.log(this.DOM.animatable);
-			this.DOM.el.addEventListener('mousemove', this.mousemoveFn);
-			this.DOM.el.addEventListener('mouseleave', this.mouseleaveFn);
-		}
-
-		mousemoveFn(ev) {
-			window.requestAnimationFrame(() => { this._layout(ev) });
-		};
-
-		mouseleaveFn(ev) {
-			requestAnimationFrame(() => {
-				for (var key in this.DOM.animatable) {
-					if (this.DOM.animatable[key]) {
-					}
-					this.DOM.animatable[key].style.transform = transformString(Object.assign({ tX: 0, tY: 0, tZ: 0, rX: 0, rY: 0, rZ: 0 }, {s: this.options.movement[key].scale}));
-				}
+		const _layout = (el, ev) => {
+	
+			let bounds = el.getBoundingClientRect();
+	
+			let txf = transformString({
+				x: move(magnitude, bounds.width, ev.offsetX),
+				y: move(magnitude, bounds.height, ev.offsetY),
 			});
-		};
-
-		_layout(ev) {
-			// Mouse position relative to the main element (this.DOM.el).
-			let relmousepos = {
-				x: ev.offsetX,
-				y: ev.offsetY,
-			};
-
-			let bounds = this.DOM.el.getBoundingClientRect();
-
-			// Movement settings for the animatable elements.
-			for (var key in this.DOM.animatable) {
-				if (this.DOM.animatable[key] == undefined || this.options.movement[key] == undefined) {
-					continue;
-				}
-
-				let t = this.options.movement[key].translation;
-				let r = this.options.movement[key].rotation;
-				let s = this.options.movement[key].scale;
-
-				// All this math does is bound + damp the transformations! Can also be done with min/max.
-				var transforms = {
-					tX: move(t.x, bounds.width, relmousepos.x),
-					tY: move(t.y, bounds.height, relmousepos.y),
-					tZ: move(t.z, bounds.height, relmousepos.y),
-					rX: move(r.x, bounds.height, relmousepos.y),
-					rY: move(r.y, bounds.width, relmousepos.x),
-					rZ: move(r.z, bounds.width, relmousepos.x),
-					s
-				}
-
-				let txf = transformString(transforms);
-
-				this.DOM.animatable[key].style.WebkitTransform = txf;
-				this.DOM.animatable[key].style.transform = txf;
-			};
+	
+			// el.style.WebkitTransform = txf;
+			requestAnimationFrame(() => { el.style.transform = txf; });
 		}
+
+		const reset = () => {
+			root.style.transform = transformString(0);
+		}
+
+		const mouseleaveFn = (ev) => {
+			requestAnimationFrame(() => {
+			});
+		}
+
+		root.addEventListener('mousemove', _layout);
+		root.addEventListener('mouseleave', mouseleaveFn);
+
+		const r = (selector) => {
+			const moveMe = root.querySelector(selector);
+			return r;
+			
+		}
+
+		return r;
 	}
 
-	window.TiltFx = TiltFx;
+	window.TiltFx = init;
 
 })();
