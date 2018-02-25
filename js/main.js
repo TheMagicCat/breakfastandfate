a57342be6db65db0da150ed99169f2bef1c22b2a
Matt Wilson <TheMagicCat@users.noreply.github.com>
2018-02-25 16:28:41 -0600

    warp

--- js/main.js
+++ js/main.js
@@ -1,43 +1,41 @@
 (function () {
 	const transformString = (v) => `translate3d(${v.x}px, ${v.y}px, 0px) scale(1)`;
 
-	// All this math does is bound + damp the transformations! Can also be done with min/max.
-	const move = (scale, bound, pos) => ((scale * 2) / bound) * pos - scale;
+	// All this math does is bound + damp the transformations! Can also be done with min/max!
+	const movement = (scale, bound, pos) => ((scale * 2) / bound) * pos - scale;
 
-	const init = (magnitude, root) => {
+	const reset = (el) => {
+		// Could also remove this key to reset to orig. CSS transform!
+		el.style.transform = transformString({ x: 0, y: 0 });
+	}
 
-		const _layout = (el, ev) => {
-	
-			let bounds = el.getBoundingClientRect();
-	
-			let txf = transformString({
-				x: move(magnitude, bounds.width, ev.offsetX),
-				y: move(magnitude, bounds.height, ev.offsetY),
-			});
-	
-			// el.style.WebkitTransform = txf;
-			requestAnimationFrame(() => { el.style.transform = txf; });
+	const layout = (el, magnitude) => (ev) => {
+
+		let bounds = el.getBoundingClientRect();
+
+		let txf = transformString({
+			x: movement(magnitude, bounds.width, ev.offsetX),
+			y: movement(magnitude, bounds.height, ev.offsetY),
+		});
+
+		requestAnimationFrame(() => { el.style.transform = txf; });
+	}
+
+	const init = (initialMagnitude, root) => {
+
+		const subscribe = (warp, leave) => {
+			root.addEventListener('mousemove', warp);
+			root.addEventListener('mouseleave', leave);
 		}
 
-		const reset = () => {
-			root.style.transform = transformString(0);
+		const r = (magnitude) => (selector) => {
+			// Multiply depth + Subscribe event + Return next chain fn!
+			const whoAmI = root.querySelector(selector);
+			subscribe(layout(whoAmI, magnitude), reset(el));
+			return r(initialMagnitude * 1.2);
 		}
 
-		const mouseleaveFn = (ev) => {
-			requestAnimationFrame(() => {
-			});
-		}
-
-		root.addEventListener('mousemove', _layout);
-		root.addEventListener('mouseleave', mouseleaveFn);
-
-		const r = (selector) => {
-			const moveMe = root.querySelector(selector);
-			return r;
-			
-		}
-
-		return r;
+		return r(initialMagnitude);
 	}
 
 	window.TiltFx = init;
